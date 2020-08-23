7ab9652abb85a41d85d47f73b89bafcd1f7e3000
Mateusz Choma <chomamateusz@gmail.com>
2020-08-23 11:36:32 +0200

    VDOM vs DOM

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 
+console.dir(document.createElement('h1'))
+console.log(React.createElement('h1', null, 'Hello React'))
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
