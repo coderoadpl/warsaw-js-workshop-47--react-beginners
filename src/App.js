4000cff9a031efadab2d4a0226d2d004af6e6892
Mateusz Choma <chomamateusz@gmail.com>
2020-08-23 12:15:15 +0200

    Iterating over elements in React

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 import React from 'react'
 import Message from './Message'
 
+const messages = [
+  {
+    text: 'Hello',
+    sender: 'Mateusz',
+    date: Date.now(),
+  },
+  {
+    text: 'hi!',
+    sender: 'Mateusz',
+    date: Date.now(),
+  }
+]
+
 const App = () => {
+
+  // const messagesElements = []
+
+  // for (let i = 0; i < messages.length; i++){
+  //   const message = messages[i]
+  //   const messageElement = <Message text={message.text} />
+  //   messagesElements.push(messageElement)
+  // }
+
+  const messagesElements = messages.map((message) => {
+    return <Message text={message.text} />
+  })
+
   return (
     <div>
-      <Message style={{ color: 'red' }} text={'Hello!'} />
-      <Message text={'Hi!'} />
-      <Message text={2 + 2} />
+      {messagesElements}
     </div>
   )
 }
