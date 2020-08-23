39e8664f81fbe9ce9981ce61dfd45375b835a30a
Mateusz Choma <chomamateusz@gmail.com>
2020-08-23 12:47:16 +0200

    Embedding map into JSX

--- src/App.js
+++ src/App.js
@@ -15,22 +15,13 @@ const messages = [
 ]
 
 const App = () => {
-
-  // const messagesElements = []
-
-  // for (let i = 0; i < messages.length; i++){
-  //   const message = messages[i]
-  //   const messageElement = <Message text={message.text} />
-  //   messagesElements.push(messageElement)
-  // }
-
-  const messagesElements = messages.map((message) => {
-    return <Message text={message.text} />
-  })
-
   return (
     <div>
-      {messagesElements}
+      {
+        messages.map((message) => {
+          return <Message text={message.text} />
+        })
+      }
     </div>
   )
 }
