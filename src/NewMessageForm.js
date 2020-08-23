48bd757013fa84e8ff6a401e577df28424c17c4e
Mateusz Choma <chomamateusz@gmail.com>
2020-08-23 13:45:19 +0200

    useState hook

--- src/NewMessageForm.js
+++ src/NewMessageForm.js
@@ -1,45 +1,14 @@
 import React from 'react'
 
-// const NewMessageForm = () => {
-//   return (
-//     <input
-//       value={'New message text!'}
-//       onChange={(event) => console.log(event.target.value)}
-//     />
-//   )
-// }
+const NewMessageForm = () => {
+  const [newMessage, setNewMessage] = React.useState('')
 
-class NewMessageForm extends React.Component {
-
-  constructor(props) {
-    super(props)
-    console.log('constructor')
-    this.state = {
-      newMessage: ''
-    }
-  }
-
-  componentDidMount() {
-    console.log('componentDidMount')
-  }
-
-  componentDidUpdate() {
-    console.log('componentDidUpdate')
-  }
-
-  render() {
-    console.log('render')
-
-    return (
-      <input
-        value={this.state.newMessage}
-        onChange={(event) => this.setState({
-          newMessage: event.target.value
-        })}
-      />
-    )
-  }
-
+  return (
+    <input
+      value={newMessage}
+      onChange={(event) => setNewMessage(event.target.value)}
+    />
+  )
 }
 
 export default NewMessageForm
