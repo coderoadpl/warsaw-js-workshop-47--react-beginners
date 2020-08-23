9f4d646d305a74d963807bcd69fbf5d346c70638
Mateusz Choma <chomamateusz@gmail.com>
2020-08-23 13:37:52 +0200

    class component and state ans setState

--- src/NewMessageForm.js
+++ src/NewMessageForm.js
@@ -1,12 +1,45 @@
 import React from 'react'
 
-const NewMessageForm = () => {
-  return (
-    <input
-      value={'New message text!'}
-      onChange={(event) => console.log(event.target.value)}
-    />
-  )
+// const NewMessageForm = () => {
+//   return (
+//     <input
+//       value={'New message text!'}
+//       onChange={(event) => console.log(event.target.value)}
+//     />
+//   )
+// }
+
+class NewMessageForm extends React.Component {
+
+  constructor(props) {
+    super(props)
+    console.log('constructor')
+    this.state = {
+      newMessage: ''
+    }
+  }
+
+  componentDidMount() {
+    console.log('componentDidMount')
+  }
+
+  componentDidUpdate() {
+    console.log('componentDidUpdate')
+  }
+
+  render() {
+    console.log('render')
+
+    return (
+      <input
+        value={this.state.newMessage}
+        onChange={(event) => this.setState({
+          newMessage: event.target.value
+        })}
+      />
+    )
+  }
+
 }
 
 export default NewMessageForm
